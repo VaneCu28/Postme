af8e3fb439720b6092a1c48141382a6a178bf315
20183l201009@utcv.edu.mx <20183l201009@utcv.edu.mx>
2022-11-26 20:18:39 -0600

    Primer commit

---
+++ src/js/firebase.js
@@ -0,0 +1,51 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBCKaYhzYEx0omsaqox1NTGrDBCvPEfN2A",
+    authDomain: "postme--app.firebaseapp.com",
+    projectId: "postme--app",
+    storageBucket: "postme--app.appspot.com",
+    messagingSenderId: "646288571723",
+    appId: "1:646288571723:web:51bdbb6e78439ee2f1fcce",
+    measurementId: "G-D7EP5JGF04"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+var db = firebase.firestore();
+// Vamos a configurar dayjs
+
+dayjs.extend(window.dayjs_plugin_relativeTime);
+const locale = {
+    name: 'es',
+    monthsShort: 'ene_feb_mar_abr_may_jun_jul_ago_sep_oct_nov_dic'.split('_'),
+    weekdays: 'domingo_lunes_martes_miércoles_jueves_viernes_sábado'.split('_'),
+    weekdaysShort: 'dom._lun._mar._mié._jue._vie._sáb.'.split('_'),
+    weekdaysMin: 'do_lu_ma_mi_ju_vi_sá'.split('_'),
+    months: 'Enero_Febrero_Marzo_Abril_Mayo_Junio_Julio_Agosto_Septiembre_Octubre_Noviembre_Diciembre'.split('_'),
+    weekStart: 1,
+    formats: {
+        LT: 'H:mm',
+        LTS: 'H:mm:ss',
+        L: 'DD/MM/YYYY',
+        LL: 'D [de] MMMM [de] YYYY',
+        LLL: 'D [de] MMMM [de] YYYY H:mm',
+        LLLL: 'dddd, D [de] MMMM [de] YYYY H:mm'
+    },
+    relativeTime: {
+        future: 'en %s',
+        past: 'hace %s',
+        s: 'unos segundos',
+        m: 'un minuto',
+        mm: '%d minutos',
+        h: 'una hora',
+        hh: '%d horas',
+        d: 'un día',
+        dd: '%d días',
+        M: 'un mes',
+        MM: '%d meses',
+        y: 'un año',
+        yy: '%d años'
+    },
+    ordinal: (n) => `${n}º`
+};
+dayjs.locale(locale, null, true);
+dayjs.locale('es');
